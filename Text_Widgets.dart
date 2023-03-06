6495c1f59c4ad0e91e13cf95d7e99e22dd646fc7
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-03-06 23:22:01 +0530

    Update Text_Widgets.dart

--- Text_Widgets.dart
+++ Text_Widgets.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(
     Text('Hello World',
     textDirection: TextDirection.ltr,
+    // ltr = left to right
+    // rtl = right to left
     textAlign: TextAlign.center,
     style: TextStyle(color: Colors.orange),)
   );
