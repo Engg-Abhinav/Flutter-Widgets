7f89e185068cd20c1f7b59718be6171ac2d566e0
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-03-06 23:29:29 +0530

    Update Text_Widgets.dart

--- Text_Widgets.dart
+++ Text_Widgets.dart
@@ -7,6 +7,15 @@ void main() {
     // ltr = left to right
     // rtl = right to left
     textAlign: TextAlign.center,
-    style: TextStyle(color: Colors.orange),)
+    style: TextStyle(color: Colors.orange),
+    overflow: TextOverflow.ellipsis,
+    textScaleFactor: 5,
+    softWrap: false,
+    // maxLines: 4,
+
+    //Text('#'
+    // SemanticsLabel: 'Abhinav Agrawal',
+    // # will be in front and in backend it will be Abhinav Agrawal
+    )
   );
 }
