d04c215cb2539624954884670b1668e4f988f308
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-03-11 23:20:59 +0530

    Text Span

--- TextSpan.dart
+++ TextSpan.dart
@@ -1,1 +1,20 @@
-!
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Sample App'),
+          ),
+          body: Center(
+            child: Text.rich(TextSpan(
+                text: 'Hello',
+                style: TextStyle(fontSize: 30),
+                children: <InlineSpan>[
+                  TextSpan(
+                      text: 'World',
+                      style: TextStyle(fontSize: 50, color: Colors.blue)),
+                  TextSpan(text: '!', style: TextStyle(fontSize: 60))
+                ])),
+          ))));
+}
